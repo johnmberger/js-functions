234e6fbf0206c3a935166854dc17ea138da68add
johnmberger <johnmartinberger@gmail.com>
2016-07-11 16:09:03 -0600

    completed exercises

---
+++ part-1.js
@@ -0,0 +1,67 @@
+// 1.
+sum = function(num1, num2){
+  return (num1 + num2);
+}
+
+// 2.
+isEqual = function(arg1, arg2) {
+  if (arg1 === arg2){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// 3.
+discountPercentage = function(origAmount, discount) {
+  if (discount > 100 || discount < 0) {
+    return 'Invalid Discount';
+  } else {
+    return (origAmount * (discount/100));
+  }
+}
+
+// 4.
+stringCapitalize = function(str) {
+  str = str.toLowerCase().split(' ');
+  for (i = 0; i < str.length; i++){
+    str[i] = str[i].split('');
+    str[i][0] = str[i][0].toUpperCase();
+    str[i] = str[i].join('');
+  } return str.join(' ');
+}
+
+// 5.
+evenNumbers = function(num){
+  for (i = 0; i < num; i++) {
+    if (i % 2 === 0) {
+      console.log(i)
+    }
+  }
+}
+
+// 6.
+isDivisible = function(arg1, arg2) {
+  if (typeof arg1 === 'number' &&  typeof arg2 === 'number') {
+    return true
+  } else {
+    return false;
+  }
+}
+
+// 7.
+oddNumbers = function(num) {
+  if (num > 40){
+    for (i=40; i<=num; i++){
+      if (i % 2 === 1){
+        console.log(i);
+      }
+    }
+  } else if (num < 40){
+    for (i=0;i<=40;i++){
+      if (i % 2 === 1){
+        console.log(i);
+      }
+    }
+  }
+}
